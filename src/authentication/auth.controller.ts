90ca78057f757be3b129a21e70144a5cff7e29fe
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-06 11:42:13 +0100

    add auth controller to check user status via APIs calls

---
+++ src/authentication/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Admin } from 'src/authorization/admin.decorator';
+import { AuthenticatedGuard } from './authenticated.guard';
+
+@Controller('auth')
+export class AuthController {
+  @Get('auth')
+  @UseGuards(AuthenticatedGuard)
+  checkAuth(): boolean {
+    return true;
+  }
+
+  @Get('admin')
+  @Admin(true)
+  checkIfAdmin(): boolean {
+    return true;
+  }
+
+}
