2f6f600ca00b85ee88d9a5a432e6cc26ab919cbc
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-07 08:50:57 +0100

    DB, cookies, session...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,63 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import pgSession = require('connect-pg-simple');
+import pg = require('pg');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const pgPool = new pg.Pool({
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+  });
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: process.env.ALLOWED_URL,
+    credentials: true,
+  });
+
+  app.use(
+    session({
+      store: new (pgSession(session))({
+        pool: pgPool,
+        createTableIfMissing: true,
+      }),
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: true,
+        secure: true,
+        httpOnly: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('User Auth Template')
+    .setDescription(
+      'User Template using passport-session, typeORM & PostgreSQL. Includes custom guards & user CRUD.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
